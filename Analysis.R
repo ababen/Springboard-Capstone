310c75d29beac7a270fb91a1b7517ff8ad0e8f03
ababen <alexbaben@gmail.com>
2016-09-11 22:18:26 -0400

    Changed path

--- Analysis.R
+++ Analysis.R
@@ -1,5 +1,8 @@
 library("dplyr")
-tweets <- read.csv("~/Springboard-Capstone/Springboard-Capstone/how-isis-uses-twitter/tweets.csv")
+tweets <- read.csv("~/Springboard-Capstone/how-isis-uses-twitter/tweets.csv")
 class(tweets)
+names(tweets)
+str(tweets)
+summary(tweets)
 View(tweets)
 # Analysis begins
