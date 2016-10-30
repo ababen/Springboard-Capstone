6d46ed19bb63a69797bb4500caa9b763b562e8b1
ababen <alexbaben@gmail.com>
2016-10-30 13:31:48 -0400

    Clean up

--- other/LearningClustering.R
+++ other/LearningClustering.R
@@ -19,31 +19,6 @@ dist(rbind(x, y), method = "euclidean")
 ### S3 method for class 'dist'
 #as.matrix(x, ...)
 
-# Reminder for myself to build a function or figure out how to make a shortcut for this.
-
 #  Using the zscore function, you can convert all the values in the data set to use the same proportional scale. See zscore for more information.
 ## Hierarchical Clustering
 # https://www.mathworks.com/help/stats/hierarchical-clustering.html?requestedDomain=www.mathworks.com
-
-Create a function distance
-Levenstein Distance/Jarcovian distance
-Above from Document Matrix
-
-Hiercha - amount of lusters
-Dendrogram - appear very dense. need to cut the roots of the tree, to get a better visualization
-Pruning the cluster
-Dont use too many cluster. the optimal number of clusters.
-
-ward distance
-
-k-means
-
-1. document matrix
-2. send function which calculates distance
-3. calculate distance and pass to hclust()
-4. k-means - recommendations around these words
-
-Friday 28th @ 9pm.
-
-Chapter 7
-By End-November
