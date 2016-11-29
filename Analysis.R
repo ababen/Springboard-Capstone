4885b7a7c3060f5a5b32bf4d43ee6435b7ff19eb
ababen <alexbaben@gmail.com>
2016-11-28 22:13:42 -0400

    Cleaned up code, added more output

--- Analysis.R
+++ Analysis.R
@@ -21,7 +21,7 @@ dtm <- DocumentTermMatrix(corpus)
 tdm.sparse <- removeSparseTerms(tdm, 0.98)
 
 plot(tdm.sparse)
-install.packages("Rgraphviz")
+# install.packages("Rgraphviz") <-remove this
 
 tweet.freq_terms <- findFreqTerms(tdm, lowfreq = 250)
 tweet.term_freq <- rowSums(as.matrix(tdm))
@@ -30,22 +30,23 @@ tdm.freq <- data.frame(term = names(tweet.term_freq), freq = tweet.term_freq)
 
 # Plotting 
 
-# plot(tdm, terms = sample(Terms(tdm), 20), corThreshold = 0.7) >- This does not work in 3.3.2
-
 library(ggplot2)
 library(plotly)
 
 ### Analysis begins ###
 
-# !!!!!!!!!!!!! I am having problems removing "the", "..." and "this" from this dataset.
-
 library(dplyr)
 
-tweets.fans = tweets.data %>% 
+tweets.data %>% 
   select(username, followers, tweets) %>%
   group_by(username) %>%
-  summarize(n_followers = max(followers), n_tweets = n())
+  summarize(n_followers = max(followers), n_tweets = n()) %>%
+  arrange(desc(n_tweets)) %>%
+  top_n(10, n_tweets) -> tweets.fans ## Add column here with percent of total.
+View(tweets.fans)
 
+#----------------------Remove this------------------------------
+k = 0
 wss = numeric(15)
 for (k in 1:15){
   wss[k] = sum(kmeans(as.matrix(tweets.fans[ , 2:3]), centers = k, nstart = 25)$withinss)
@@ -58,7 +59,7 @@ tweets.fans %>%
   ggplot(aes(n_tweets, n_followers)) +
     geom_point(aes(color = factor(ISIS_Cluster$cluster))) +
     geom_text(aes(label = ifelse(n_followers > 7000 | n_tweets > 7000, as.character(username), " ")), vjust = 1.5)
-
+#----------------------Remove this------------------------------
 
 # Look up all #hashtags by frequency
 
@@ -89,33 +90,23 @@ View(tweets.users)
 
 library(lubridate)
 
-tweets.time = tweets.data$time
-tweets.time = mdy_hm(tweets.time)
-# tweets.time = as.data.frame(tweets.data$time)
-tweets.time$year = year(tweets.time)
-tweets.time$month = month(tweets.time) 
-tweets.time$day = day(tweets.time)
+ tweets.data %>%
+  mutate(time = parse_date_time(time, orders="%m/%d/%Y %H:%M"),
+         year = format(time, "%Y"),
+         month = format(time, "%m"),
+         day = format(time, "%d"), 
+         hour = format(time, "%H")) -> tweets.time
 
-# tweets.time = mutate(tweets.time, parse_date_time(tweets.time, orders = "%m/%d/%Y %H:%M"), year = format(tweets.data$time, "%Y"), month = format(tweets.data$time, "%m"), day = format(tweets.data$time, "%d"), hour = format(tweets.data$time, "%H"))
-
-ggplot(tweets.time, aes(day(tweets.time))) + 
-    geom_bar(fill = "lightblue") + 
-    theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
-    ggtitle("# of tweets published daily") + xlab("") + ylab("")
-
-ggplotly(plot.time)
-
-# ??? # of accounts by month
-
-# ??? Diagram of words
-
-# ??? Diagram of #hashtags
-
-# Classification: Retweets, URL (these can be changed to binary variables)
-# Classifiers to try: MultinomialNB, KNeighbors, RandomForest GradientBoosting
-
-# !!!!!!!!!!!!! I am having problems removing "the", "..." and "this" from this dataset.
-
+tweets.time %>%
+   group_by(year) %>%
+   ggplot(aes(month, fill=year)) +
+   geom_bar(position="stack") +
+   theme(axis.text=element_text(size=14), 
+         axis.title=element_text(size=16), 
+         legend.title=element_text(size=14),
+         legend.key.size=unit(1,'cm'),
+         legend.text=element_text(size=14)) 
+ 
 ggplot(tdm.freq, aes(x = term, y = freq)) + #reorder(term.freq)
   geom_bar(stat = "identity") + 
   coord_flip() + 
@@ -129,62 +120,40 @@ wordcloud(words = tdm.freq$term, freq = tdm.freq$freq, random.order = FALSE, rot
 
 ### Code from Amit
 
-library(tm)
+# library(tm) <-remove this
 
 tdm.sparse = removeSparseTerms(tdm, 0.98)
 
 # #The above code will give you a distance matrix between the main keywords
 distMatrix = dist(scale(as.matrix(tdm.sparse)),method = "cosine")
 
-library(igraph)
-
 fit = hclust(distMatrix,method = 'ward.D2')
 fit.cut = cutree(fit, k = 2)
 plot(fit)
-plot(fit.cut)
 
-k = 8
-kMeans = kmeans(tdm.sparse, k)
-round(kMeans$centers, digits = 3)
+#----------------------Remove this------------------------------
+#
+#k = 3
+#kMeans = kmeans(tdm.sparse, k)
+#round(kMeans$centers, digits = 3)
 
-for(i in 1:k){
-  cat(paste(" cluster", i, ": ", sep = ""))
-  s = sort(kMeans$centers[i, ], decreasing = T)
-  cat(names(s)[1:3], "\n")
-}
+#for(i in 1:k){
+#  cat(paste(" cluster", i, ": ", sep = ""))
+#  s = sort(kMeans$centers[i, ], decreasing = T)
+#  cat(names(s)[1:3], "\n")
+#}
+#----------------------Remove this------------------------------
 
-# ??? Look up all #hashtags by frequency
-# ??? Look up all @users by frequency
+library(igraph)
 
-# ??? Tweets by month, tweets by day, Most followed users and location, 
 
+library(networkD3)
+
+#---------------------- Things to try --------------------------
 # ??? Diagram of words
 # ??? Diagram of #hashtags
-
+# https://eight2late.wordpress.com/2015/09/29/a-gentle-introduction-to-topic-modeling-using-r/
+# ??? # of accounts by month
 # Classification: Retweets, URL (these can be changed to binary variables)
-
 # Classifiers to try: MultinomialNB, KNeighbors, RandomForest GradientBoosting
-
-# AMB: I dont now where I was going with this.
-# Train the model
-## mat = as.matrix(tdm)
-## classifier = naiveBayes(mat[1:10,], as.factor(tdm[1:10,2]))
-
-# Test the validity
-## predicted = predict(classifier, mat[11:15,]); predicted
-## table(tdm[11:15, 2], predicted)
-## recall_accuracy(tdm[11:15, 2], predicted)
-
-# Show difference Cross Validation Mean Scores between Naive Bayes, KN Neighbors, Random Forrest and Gradient Boosting
-
-# Develop an ISIS vocabulary for sentiment analysis.
-
 # Kaggle: Sentiment Analysis: Which clergy do pro-ISIS fanboys quote the most and which ones do they hate the most? Search the tweets for names of prominent clergy and classify the tweet as positive, negative, or neutral and if negative, include the reasons why. Examples of clergy they like the most: "Anwar Awlaki", "Ahmad Jibril", "Ibn Taymiyyah", "Abdul Wahhab". Examples of clergy that they hate the most: "Hamza Yusuf", "Suhaib Webb", "Yaser Qadhi", "Nouman Ali Khan", "Yaqoubi".
-
-# Next step: work on using Predictive Coding and SentimentAnalysis.R on the project. #
-
-# To Do
-# 1. Calculate sentiment for each tweet.
-#   a. Use keyword dictionaries to determine postivie/neutral/negative words.
-# 2. Add up sentiment over time.
-# 3. Plot time series
